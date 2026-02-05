34b794fb5d04b3fd884ae61b38648da5a37bcb65
valenmarton <martonvalentin97@gmail.com>
2026-02-05 09:11:27 +0100

    vim-illuminate error fix

--- lua/plugins/illuminate-underscore.lua
+++ lua/plugins/illuminate-underscore.lua
@@ -1,9 +1,9 @@
 return {
 	"RRethy/vim-illuminate",
 	event = "BufReadPost",
-	opts = {
-		-- optional settings
-		delay = 200,
-		filetypes_denylist = { "TelescopePrompt", "vim" },
-	},
+	-- opts = {
+	-- optional settings
+	-- delay = 200,
+	-- filetypes_denylist = { "TelescopePrompt", "vim" },
+	-- },
 }
